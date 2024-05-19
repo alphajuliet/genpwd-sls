ed3140b8ab27213e138671a3e4965c9b0db2da8e
alphajuliet <andrew@alphajuliet.com>
2024-05-19 15:44:27 +1000

    Upgrade CORS implementation

--- src/genpwd.js
+++ src/genpwd.js
@@ -15,8 +15,8 @@ const generators = [
 const Info = {
   name: "GenPwd",
   author: "AndrewJ",
-  version: "3.0.0",
-  date: "2018-09-22",
+  version: "3.0.1",
+  date: "2024-05-13",
   info: "GenPwd is a simple password generator.",
   aboutText: function () {
     let str = this.name + " v" + this.version;
@@ -28,7 +28,7 @@ const Info = {
 };
 
 // Available generators
-const generatorList = [
+const availableGenerators = [
   { id: 0, name: "Generator 0" },
   { id: 1, name: "Generator 1" },
   { id: 2, name: "Generator 2" },
@@ -36,7 +36,11 @@ const generatorList = [
 ]
 
 // Generate a list of random words from the chosen generator.
-const generate = async (event) => {
+const generate = async (event, context, callback) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return handlePreflight();
+  }
+
   const params = event.queryStringParameters || {};
   const genId = parseInt(params.genId) || 3;
   const generatorModule = generators[genId];
@@ -53,9 +57,49 @@ const generate = async (event) => {
     capitals: capitals
   };
 
-  const resp = R.map(() => generator.randomWord(strength, options), R.range(0, nwords));
-  return resp;
+  const wordList = R.map(() => generator.randomWord(strength, options), R.range(0, nwords));
+  const httpResp = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify({words: wordList})
+  }
+  callback(null, httpResp);
 }
+
+const generatorList = async (event, context, callback) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return handlePreflight();
+  }
+
+  const httpResp = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify({generators: availableGenerators})
+  }
+  callback(null, httpResp);
+}
+
+const handlePreflight = () => {
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      // 'Access-Control-Allow-Credentials': true
+    },
+    body: null
+  }
+}
+
 exports.info = Info;
 exports.generators = generatorList;
 exports.generate = generate;
